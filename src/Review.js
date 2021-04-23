ecbb480cca6ab900bc12a77ad9e82813048f6186
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-22 20:34:37 -0700

    Setup onClick for random person button.
    - Calculate an appropriate random number and then set the index state to the new random number.

--- src/Review.js
+++ src/Review.js
@@ -29,6 +29,14 @@ const Review = ({}) => {
     });
   }
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber))
+  }
+
   return <article className="review">
     <div className="img-container">
       <img src={image} alt={name} className="person-img" />
@@ -47,7 +55,7 @@ const Review = ({}) => {
         <FaChevronRight/>
       </button>
     </div>
-    <button className="random-btn">surprise me</button>
+    <button className="random-btn" onClick={randomPerson}>surprise me</button>
   </article>
 };
 
